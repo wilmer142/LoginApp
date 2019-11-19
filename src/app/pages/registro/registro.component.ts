11051141660f44ac2a0a836bfe5dec402659f653
wilmer142 <wilmercollege@gmail.com>
2019-11-19 12:13:49 -0500

    Implementing register form validations

--- src/app/pages/registro/registro.component.ts
+++ src/app/pages/registro/registro.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { usuarioModel } from 'src/app/models/usuario.model';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-registro',
@@ -7,10 +9,18 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegistroComponent implements OnInit {
 
+  usuario: usuarioModel;
 
   constructor() { }
 
-  ngOnInit() { }
+  ngOnInit() {
+    this.usuario = new usuarioModel();
+  }
 
+  onSubmit(form:NgForm){
+    console.log(form);
+    if (!form.valid) { return; }
+    console.log(this.usuario);
+  }
 
 }
